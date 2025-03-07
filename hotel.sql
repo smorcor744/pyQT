da7f343f22749cf605e66e7b3fff8d1e605466fd
ruben asua <rubenasuasoto@gmail.com>
2025-03-07 11:58:11 +0100

    new file:   BD_reservas.py 	new file:   __pycache__/BD_reservas.cpython-312.pyc 	new file:   fondo.jpg 	modified:   habitacion.ui 	modified:   hotel.db 	new file:   hotel.sql 	new file:   reservas.py 	new file:   reservas.ui 	new file:   reservas_ui.py

---
+++ hotel.sql
@@ -0,0 +1,50 @@
+-- Crear tabla clientes
+CREATE TABLE IF NOT EXISTS clientes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    apellido1 TEXT NOT NULL,
+    apellido2 TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    dni TEXT UNIQUE NOT NULL
+);
+
+-- Crear tabla habitaciones
+CREATE TABLE IF NOT EXISTS habitaciones (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    numero TEXT UNIQUE NOT NULL,
+    tipo TEXT NOT NULL CHECK(tipo IN ('Individual', 'Doble')),
+    precio_noche REAL NOT NULL,
+    disponible BOOLEAN DEFAULT TRUE
+);
+
+-- Crear tabla reservas
+CREATE TABLE IF NOT EXISTS reservas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email_cliente TEXT NOT NULL,
+    numero_habitacion TEXT NOT NULL,
+    fecha_checkin DATE NOT NULL,
+    fecha_checkout DATE NOT NULL,
+    estado TEXT DEFAULT 'Pendiente' CHECK(estado IN ('Pendiente', 'Confirmada', 'Cancelada', 'Finalizada')),
+    FOREIGN KEY (email_cliente) REFERENCES clientes(email) ON DELETE CASCADE,
+    FOREIGN KEY (numero_habitacion) REFERENCES habitaciones(numero) ON DELETE CASCADE
+);
+
+-- Crear tabla pagos
+CREATE TABLE IF NOT EXISTS pagos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_reserva INTEGER NOT NULL,
+    monto REAL NOT NULL,
+    metodo_pago TEXT NOT NULL CHECK(metodo_pago IN ('Tarjeta', 'Efectivo')),
+    fecha_pago DATE NOT NULL,
+    FOREIGN KEY (id_reserva) REFERENCES reservas(id) ON DELETE CASCADE
+);
+
+-- Crear tabla empleados
+CREATE TABLE IF NOT EXISTS empleados (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL,
+    cargo TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    telefono TEXT NOT NULL
+);
---
+++ reservas_ui.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'reservas.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDateEdit, QHeaderView, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QTableWidget, QTableWidgetItem, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1050, 724)
+        MainWindow.setMinimumSize(QSize(1050, 680))
+        MainWindow.setStyleSheet(u"background-color:;")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(140, 10, 71, 41))
+        font = QFont()
+        font.setPointSize(14)
+        self.label.setFont(font)
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(70, 100, 111, 16))
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(70, 170, 81, 16))
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(70, 240, 91, 16))
+        self.label_5 = QLabel(self.centralwidget)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(70, 30, 71, 16))
+        self.label_6 = QLabel(self.centralwidget)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(70, 310, 71, 16))
+        self.bt_registrar_reserva = QPushButton(self.centralwidget)
+        self.bt_registrar_reserva.setObjectName(u"bt_registrar_reserva")
+        self.bt_registrar_reserva.setGeometry(QRect(130, 420, 75, 24))
+        self.bt_registrar_reserva.setStyleSheet(u"background-color: rgb(0, 85, 0);")
+        self.textNumero = QLineEdit(self.centralwidget)
+        self.textNumero.setObjectName(u"textNumero")
+        self.textNumero.setGeometry(QRect(70, 125, 241, 31))
+        self.textDisponible = QLineEdit(self.centralwidget)
+        self.textDisponible.setObjectName(u"textDisponible")
+        self.textDisponible.setGeometry(QRect(70, 330, 241, 31))
+        self.textEmail = QLineEdit(self.centralwidget)
+        self.textEmail.setObjectName(u"textEmail")
+        self.textEmail.setGeometry(QRect(70, 50, 241, 31))
+        self.bt_mostrar_reservas = QPushButton(self.centralwidget)
+        self.bt_mostrar_reservas.setObjectName(u"bt_mostrar_reservas")
+        self.bt_mostrar_reservas.setGeometry(QRect(630, 510, 121, 31))
+        self.bt_mostrar_reservas.setStyleSheet(u"background-color: rgb(0, 85, 255);")
+        self.tablaReservas = QTableWidget(self.centralwidget)
+        self.tablaReservas.setObjectName(u"tablaReservas")
+        self.tablaReservas.setEnabled(True)
+        self.tablaReservas.setGeometry(QRect(390, 10, 611, 481))
+        self.textIdDelete = QLineEdit(self.centralwidget)
+        self.textIdDelete.setObjectName(u"textIdDelete")
+        self.textIdDelete.setGeometry(QRect(80, 560, 181, 31))
+        self.label_7 = QLabel(self.centralwidget)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setGeometry(QRect(100, 540, 141, 16))
+        self.bt_eliminar_reservas = QPushButton(self.centralwidget)
+        self.bt_eliminar_reservas.setObjectName(u"bt_eliminar_reservas")
+        self.bt_eliminar_reservas.setGeometry(QRect(130, 600, 75, 24))
+        self.bt_eliminar_reservas.setStyleSheet(u"background-color: rgb(255, 0, 0);")
+        self.bt_actualizar_reserva = QPushButton(self.centralwidget)
+        self.bt_actualizar_reserva.setObjectName(u"bt_actualizar_reserva")
+        self.bt_actualizar_reserva.setGeometry(QRect(130, 460, 75, 24))
+        self.bt_actualizar_reserva.setStyleSheet(u"background-color: rgb(255, 170, 0);")
+        self.textUserID = QLineEdit(self.centralwidget)
+        self.textUserID.setObjectName(u"textUserID")
+        self.textUserID.setGeometry(QRect(240, 460, 61, 31))
+        self.label_8 = QLabel(self.centralwidget)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setGeometry(QRect(220, 460, 21, 16))
+        self.bt_atras = QPushButton(self.centralwidget)
+        self.bt_atras.setObjectName(u"bt_atras")
+        self.bt_atras.setGeometry(QRect(450, 610, 75, 51))
+        icon = QIcon(QIcon.fromTheme(u"go-previous"))
+        self.bt_atras.setIcon(icon)
+        self.textIdBuscar = QLineEdit(self.centralwidget)
+        self.textIdBuscar.setObjectName(u"textIdBuscar")
+        self.textIdBuscar.setGeometry(QRect(790, 560, 181, 31))
+        self.label_9 = QLabel(self.centralwidget)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setGeometry(QRect(810, 540, 141, 16))
+        self.bt_buscar_reserva = QPushButton(self.centralwidget)
+        self.bt_buscar_reserva.setObjectName(u"bt_buscar_reserva")
+        self.bt_buscar_reserva.setGeometry(QRect(840, 600, 75, 24))
+        self.bt_buscar_reserva.setStyleSheet(u"background-color: rgb(170, 0, 255);")
+        self.checkIn = QDateEdit(self.centralwidget)
+        self.checkIn.setObjectName(u"checkIn")
+        self.checkIn.setGeometry(QRect(70, 200, 241, 31))
+        self.checkOut = QDateEdit(self.centralwidget)
+        self.checkOut.setObjectName(u"checkOut")
+        self.checkOut.setGeometry(QRect(70, 270, 241, 31))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Reservas", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Numero habitacion", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Fecha checkin", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"fecha_checkout", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Email", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Estado", None))
+        self.bt_registrar_reserva.setText(QCoreApplication.translate("MainWindow", u"Registrar", None))
+        self.bt_mostrar_reservas.setText(QCoreApplication.translate("MainWindow", u"Mostrar Reservas", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Introduce email del cliente", None))
+        self.bt_eliminar_reservas.setText(QCoreApplication.translate("MainWindow", u"Eliminar", None))
+        self.bt_actualizar_reserva.setText(QCoreApplication.translate("MainWindow", u"Actualizar", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"ID", None))
+        self.bt_atras.setText("")
+        self.textIdBuscar.setText("")
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"Introduce email del cliente", None))
+        self.bt_buscar_reserva.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
+    # retranslateUi
+
