c1ce5541cda0346d14f0c93ca7b68ab35f61623a
sebasdelalv340 <148891336+sebasdelalv340@users.noreply.github.com>
2025-03-09 20:20:04 +0100

    cambios login y principal

--- FireBase/Login.py
+++ FireBase/Login.py
@@ -3,6 +3,21 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5.uic import loadUi
 from firebase_config import auth
 
+def ejecutar_sql():
+    """Ejecuta el archivo .sql que contiene las instrucciones de creación de tablas."""
+    try:
+        with open("hotel.sql", "r") as archivo_sql:
+            sql_script = archivo_sql.read()
+
+        conexion = sqlite3.connect('hotel.db')
+        cursor = conexion.cursor()
+        cursor.executescript(sql_script)
+        conexion.commit()
+        conexion.close()
+        print("Base de datos configurada correctamente.")
+    except Exception as e:
+        print(f"Error al ejecutar el script SQL: {e}")
+
 class Login(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -88,6 +103,7 @@ class CreateAcc(QMainWindow):
         self.hide()
 
 if __name__ == "__main__":
+    ejecutar_sql()
     app = QApplication(sys.argv)
     login_window = Login()
     login_window.show()
