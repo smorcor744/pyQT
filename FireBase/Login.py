d98597834440cff23e0c46ca712b171bba8dfeae
sebasdelalv340 <148891336+sebasdelalv340@users.noreply.github.com>
2025-03-09 20:10:52 +0100

    commit login

--- FireBase/Login.py
+++ FireBase/Login.py
@@ -87,46 +87,6 @@ class CreateAcc(QMainWindow):
         self.createacc.show()
         self.hide()
 
-class CreateAcc(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setStyleSheet("""
-            QMainWindow {
-                background-image: url(fondo.jpg);
-                background-position: center;
-                background-repeat: no-repeat;
-            }
-        """)
-        loadUi("FireBase/ui/createacc.ui", self)
-        self.signupbutton.clicked.connect(self.createaccfunction)
-        self.password.setEchoMode(2)
-        self.confirmpass.setEchoMode(2)
-        self.login.clicked.connect(self.gotologin)
-
-    def createaccfunction(self):
-        email = self.email.text()
-        password = self.password.text()
-        confirm_password = self.confirmpass.text()
-        if not email or not password or not confirm_password:
-            QMessageBox.warning(self, "Input Error", "Please fill in all fields.")
-            return
-        if password != confirm_password:
-            QMessageBox.warning(self, "Password Error", "Passwords do not match. Please try again.")
-            return
-        try:
-            auth.create_user_with_email_and_password(email, password)
-            QMessageBox.information(self, "Success", "Account created successfully!")
-            self.login = Login()
-            self.login.show()
-            self.hide()
-        except:
-            QMessageBox.critical(self, "Signup Error", "Failed to create account. Please try again.")
-
-    def gotologin(self):
-        self.login = Login()
-        self.login.show()
-        self.hide()
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     login_window = Login()
